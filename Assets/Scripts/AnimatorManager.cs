ae1ac5416641ed7f12449744c523222ad165004e
Emerson Mistico <102876556+Emerson-Mistico@users.noreply.github.com>
2025-08-27 14:59:17 -0300

    Run, Idle , dead
    Implement some animations

---
+++ Assets/Scripts/AnimatorManager.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+
+public class AnimatorManager : MonoBehaviour
+{
+    public Animator animator;
+    public List<AnimatorSetup> animatorSetups;
+    
+    public enum AnimationType
+    {
+        IDLE,
+        RUN,
+        DEAD
+    }
+    public void PlayAnimation(AnimationType type)
+    {
+        foreach (var animation in animatorSetups)
+        {
+            if (animation.type == type)
+            {
+                animator.SetTrigger(animation.triggerName);
+                break;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) 
+        { 
+            PlayAnimation(AnimationType.RUN);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            PlayAnimation(AnimationType.DEAD);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            PlayAnimation(AnimationType.IDLE);
+        }
+    }
+}
+
+[System.Serializable]
+
+public class AnimatorSetup
+{
+    public AnimatorManager.AnimationType type;
+    public string triggerName;
+
+}
