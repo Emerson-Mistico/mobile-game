3a13e7e00251073714e86e0b13dc90189a4b375a
Emerson Mistico <102876556+Emerson-Mistico@users.noreply.github.com>
2025-08-30 09:24:26 -0300

    Piece levels
    Create some piece levels to randomize

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -5,23 +5,48 @@ using System.Collections.Generic;
 public class LevelManager : MonoBehaviour
 {
 
+    [Header("Level Setup")]
     public Transform container;
-
     public List<GameObject> levelList;
 
+    // privates
     [SerializeField] private int _levelIndex;
+    private GameObject _currentLevel;
 
     private void Awake()
     {
-        SpawnLevel();
+        SpawnNextLevel();
     }
 
-    private void SpawnLevel()
+    private void SpawnNextLevel()
     {
-        var currentLevel = Instantiate(levelList[_levelIndex], container);
-        currentLevel.transform.localPosition = Vector3.zero;
+        if (_currentLevel != null)
+        {
+            Destroy(_currentLevel);
+            _levelIndex++;
+        }
+
+        if (_levelIndex >= levelList.Count)
+        {
+            ResetLevelIndex();
+        }
+
+        _currentLevel = Instantiate(levelList[_levelIndex], container);
+        _currentLevel.transform.localPosition = Vector3.zero;
     }
 
+    private void ResetLevelIndex()
+    {
+        _levelIndex = 0;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            SpawnNextLevel();
+        }
+    }
 
 
 }
